748d211758ff248f1917b36d010fd3472c00af00
svc-student <svc-student@student.directory.jccc>
2017-02-01 13:12:15 -0600

    Files have been submitted

---
+++ Combine.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p "Blank.unity" "Red.unity" "Blue.unity" "End.unity"
+
+"C:\Program Files\Unity\Editor\Unity.exe" -batchmode -projectPath C:\Users\svc-student\Desktop\ProjectOne -buildWindowsPlayer C:\Users\svc-student\Desktop\build.exe -quit
+
+
